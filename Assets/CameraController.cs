fcc15cd814a3b81fcab4c00b3d742189d17fd80c
Christian Gramling <gramlinc@uci.edu>
2018-04-11 15:53:48 -0700

    Camera and stuff

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject toFollow;
+
+    private Vector3 offset;
+    private int count;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - toFollow.transform.position;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = toFollow.transform.position + offset;
+    }
+}
