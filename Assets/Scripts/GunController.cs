b60109d2da3a885ce453b94914954a966e41fafc
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-04-11 18:25:47 -0700

    Add Airborn Rocket Limit

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -6,21 +6,30 @@ public class GunController : MonoBehaviour {
     public GameObject projectile, projectileParent;
     public float jumpForce, groundedCheckDist, deadZoneThreshold, walkSpeed, airWalkSpeed, explosionRadius, explosionForce, fireRate;
     public int numUpdatesToIgnoreGroundedCheck = 5;
+    public SpriteRenderer rocket1, rocket2, rocket3;
+    public Color active, inactive;
     private float timeSinceLastProjectile;
     private int groundedCheckReset;
     private Rigidbody2D rb;
     private bool isGrounded = false;
+    private int numRocketsLeft = 3;
+    private bool hasFired = false;
     private Quaternion gunRotation = new Quaternion();//temp variable reset every frame that is the same as the transform rotation
 	// Use this for initialization
 	void Start () {
         groundedCheckReset = numUpdatesToIgnoreGroundedCheck;
         rb = GetComponent<Rigidbody2D>();
         timeSinceLastProjectile = fireRate;
+        rocket1.color = active;
+        rocket2.color = active;
+        rocket3.color = active;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Vector2.Distance(new Vector2(), new Vector2(Input.GetAxis("Joystick_2_x"), Input.GetAxis("Joystick_2_y"))) >= deadZoneThreshold)//use circle and own deadzone calc to make circular deadzone
+        //Debug.Log(Vector2.Distance(new Vector2(), new Vector2(Input.GetAxisRaw("Joystick_2_x"), Input.GetAxisRaw("Joystick_2_y")))+"\n" + new Vector2(Input.GetAxisRaw("Joystick_2_x"), Input.GetAxisRaw("Joystick_2_y")).magnitude);
+        
+        if (Vector2.Distance(new Vector2(), new Vector2(Input.GetAxisRaw("Joystick_2_x"), Input.GetAxisRaw("Joystick_2_y"))) >= deadZoneThreshold)//use circle and own deadzone calc to make circular deadzone
         {
             //determine the angle for the gun to be facing according to player input iff player gave strong enough input
             gunRotation.eulerAngles = new Vector3(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(Input.GetAxis("Joystick_2_x"), Input.GetAxis("Joystick_2_y")));
@@ -34,6 +43,10 @@ public class GunController : MonoBehaviour {
             if (test.collider != null) {
                // Debug.Log(test.collider.gameObject.name);
                 isGrounded = true;
+                rocket1.color = active;
+                rocket2.color = active;
+                rocket3.color = active;
+                numRocketsLeft = 3;
             }
         }
         //the jump itself
@@ -85,22 +98,47 @@ public class GunController : MonoBehaviour {
             timeSinceLastProjectile += Time.deltaTime;
         }
         //shooting projectiles
-        if (Input.GetAxis("RightTrigger") != 0 && timeSinceLastProjectile >= fireRate)
+        if (Input.GetAxis("RightTrigger") != 0)
         {
-            //Debug.Log("angle: " + (this.transform.rotation.eulerAngles.z - 90) + "\nexpanded vector:" + new Vector2(Mathf.Cos(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90)), Mathf.Sin(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90))));
-            RaycastHit2D test = Physics2D.Raycast((Vector2)(transform.position), new Vector2(Mathf.Cos(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90)), Mathf.Sin(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90))), distance: Mathf.Infinity, layerMask: 1);
-            if (test.collider != null)
+            if (timeSinceLastProjectile >= fireRate && !hasFired)
             {
-                
-                //Debug.Log(test.collider.gameObject.name + "   " + Vector2.Distance((Vector2)(test.transform.position), (Vector2)transform.position));
-                projectile.transform.position = new Vector3(test.point.x, test.point.y, projectile.transform.position.z);
-                timeSinceLastProjectile = 0;
-                if (Vector2.Distance(test.point, (Vector2)transform.position) < explosionRadius)
+                hasFired = true;
+                //Debug.Log("angle: " + (this.transform.rotation.eulerAngles.z - 90) + "\nexpanded vector:" + new Vector2(Mathf.Cos(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90)), Mathf.Sin(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90))));
+                RaycastHit2D test = Physics2D.Raycast((Vector2)(transform.position), new Vector2(Mathf.Cos(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90)), Mathf.Sin(Mathf.Deg2Rad * (this.transform.rotation.eulerAngles.z - 90))), distance: Mathf.Infinity, layerMask: 1);
+                if (!isGrounded)
                 {
-                    rb.AddForce(-explosionForce * (test.point - (Vector2)this.transform.position).normalized, ForceMode2D.Impulse);
+                    
+                    if (numRocketsLeft == 1)
+                        rocket1.color = inactive;
+                    if (numRocketsLeft == 2)
+                        rocket2.color = inactive;
+                    if (numRocketsLeft == 3)
+                        rocket3.color = inactive;
+
                 }
-                
+                if (test.collider != null)
+                {
+
+                    //Debug.Log(test.collider.gameObject.name + "   " + Vector2.Distance((Vector2)(test.transform.position), (Vector2)transform.position));
+                    projectile.transform.position = new Vector3(test.point.x, test.point.y, projectile.transform.position.z);
+                    timeSinceLastProjectile = 0;
+
+                    if (Vector2.Distance(test.point, (Vector2)transform.position) < explosionRadius && numRocketsLeft > 0)
+                    {
+                        if (isGrounded)
+                            numRocketsLeft += 1;
+                        isGrounded = false;
+                        groundedCheckReset = numUpdatesToIgnoreGroundedCheck;
+                        rb.AddForce(-explosionForce * (test.point - (Vector2)this.transform.position).normalized, ForceMode2D.Impulse);
+                    }
+
+                }
+                if(!isGrounded)
+                numRocketsLeft -= 1;
             }
+        }else
+        {
+            hasFired = false;
         }
     }
 }
