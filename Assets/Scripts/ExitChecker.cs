cd0601a3a184a7ad1a348659100153a0afa4789f
Christian Gramling <gramlinc@uci.edu>
2018-05-02 16:26:06 -0700

    New Exit

---
+++ Assets/Scripts/ExitChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitChecker : MonoBehaviour {
+
+    public int tp_count  = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+                tp_count++;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && tp_count <2)
+           tp_count--;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ExitController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ExitController : NetworkBehaviour  {
+
+    public int ready_count = 0;
+    //public List<Vector2> futureExits;
+    public Vector3 nextRespawn;
+    private Transform tp;
+    private GameObject rp;
+    //public int currentLevel;
+	void Start () {
+        tp = this.transform.GetChild(0);
+        rp = GameObject.Find("RespawnPoint");
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+    
+       if (ready_count >=2)
+        {
+
+            rp.transform.position = nextRespawn;
+            tp.gameObject.SetActive(false);
+            this.enabled = false;
+   
+        }
+
+    }
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+                ready_count++;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && ready_count <2)
+           ready_count--;
+    }
+}
